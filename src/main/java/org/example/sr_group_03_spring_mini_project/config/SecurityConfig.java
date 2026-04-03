0d9e1c219b13bdebdffe1b7f022ca069f29c7974
RoaTheDev <roachamreun15@proton.me>
2026-04-03 15:36:11 +0700

    init security v2

--- src/main/java/org/example/sr_group_03_spring_mini_project/config/SecurityConfig.java
+++ src/main/java/org/example/sr_group_03_spring_mini_project/config/SecurityConfig.java
@@ -3,10 +3,17 @@ package org.example.sr_group_03_spring_mini_project.config;
 import org.example.sr_group_03_spring_mini_project.filter.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -15,9 +22,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
+    private final UserDetailsService userDetailsService;
 
-    public SecurityConfig(JwtFilter jwtFilter) {
+    public SecurityConfig(JwtFilter jwtFilter, UserDetailsService userDetailsService) {
         this.jwtFilter = jwtFilter;
+        this.userDetailsService = userDetailsService;
     }
 
     @Bean
@@ -31,4 +40,25 @@ public class SecurityConfig {
                 .build();
     }
 
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authProvider() {
+        DaoAuthenticationProvider daoAuth = new DaoAuthenticationProvider(userDetailsService);
+        daoAuth.setPasswordEncoder(passwordEncoder());
+        return daoAuth;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) {
+        try {
+            return authConfig.getAuthenticationManager();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/org/example/sr_group_03_spring_mini_project/utils/JwtUtils.java
+++ src/main/java/org/example/sr_group_03_spring_mini_project/utils/JwtUtils.java
@@ -1,8 +1,65 @@
 package org.example.sr_group_03_spring_mini_project.utils;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.UUID;
 
 @Component
 public class JwtUtils {
 
+    @Value("${jwt.secret_key}")
+    private String SECRET_KEY;
+    private final Instant ISSUED_AT = Instant.now();
+
+
+    public SecretKey secretKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .id(UUID.randomUUID().toString())
+                .subject(userDetails.getUsername())
+                .issuer("group-3.io")
+                .signWith(secretKey())
+                .issuedAt(Date.from(ISSUED_AT))
+                .expiration(Date.from(ISSUED_AT.plus(10, ChronoUnit.HOURS)))
+                .compact();
+    }
+
+    private Claims extractAllClaims(String jwt) {
+        return Jwts.parser()
+                .verifyWith(secretKey())
+                .build()
+                .parseSignedClaims(jwt)
+                .getPayload();
+    }
+
+    public String extractEmail(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+
+    public Date extractExpDate(String token) {
+        return extractAllClaims(token).getExpiration();
+    }
+
+    public boolean isTokenExp(String token) {
+        return extractExpDate(token).before(Date.from(ISSUED_AT));
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        String email = extractEmail(token);
+        return email.equals(userDetails.getUsername()) && !isTokenExp(token);
+    }
 }
