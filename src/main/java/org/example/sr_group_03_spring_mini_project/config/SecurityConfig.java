f8723f6bfd14c4a65edfc8db9ae10f36523cecad
RoaTheDev <roachamreun15@proton.me>
2026-04-03 14:48:18 +0700

    setup security

---
+++ src/main/java/org/example/sr_group_03_spring_mini_project/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package org.example.sr_group_03_spring_mini_project.config;
+
+import org.example.sr_group_03_spring_mini_project.filter.JwtFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> req.requestMatchers(""))
+                .build();
+    }
+
+}
---
+++ src/main/java/org/example/sr_group_03_spring_mini_project/utils/JwtUtils.java
@@ -0,0 +1,8 @@
+package org.example.sr_group_03_spring_mini_project.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtils {
+
+}
