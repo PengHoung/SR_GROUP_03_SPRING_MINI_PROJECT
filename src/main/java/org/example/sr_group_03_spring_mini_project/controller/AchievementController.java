725dd8d55c1d79772568d10192eca8085a21cc7b
PengHoung <cheateng678@gmail.com>
2026-04-03 15:43:40 +0700

    add anotation

--- src/main/java/org/example/sr_group_03_spring_mini_project/controller/AchievementController.java
+++ src/main/java/org/example/sr_group_03_spring_mini_project/controller/AchievementController.java
@@ -1,4 +1,7 @@
 package org.example.sr_group_03_spring_mini_project.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AchievementController {
 }
