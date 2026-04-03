57839445c284d185dc39cee5792da882b47b8668
Uy-Chakriya <uychakriya002@gmail.com>
2026-04-03 14:59:21 +0700

    merge udate conflict

--- src/main/java/org/example/sr_group_03_spring_mini_project/model/entity/FileMetadata.java
+++ src/main/java/org/example/sr_group_03_spring_mini_project/model/entity/FileMetadata.java
@@ -1,15 +1,15 @@
 package org.example.sr_group_03_spring_mini_project.model.entity;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-public class FileMetadata {
+@AllArgsConstructor
+public class FileMetaData {
     private String fileName;
-    private String fileUrl;
     private String fileType;
+    private String fileUrl;
     private Long fileSize;
-
 }
