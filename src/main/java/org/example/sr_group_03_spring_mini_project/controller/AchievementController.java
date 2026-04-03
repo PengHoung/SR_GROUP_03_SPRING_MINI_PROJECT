32c1fca10fff190b96ae75cc48fd1c099b8c196b
channsreypich <chhunchansreypich645@gmail.com>
2026-04-03 16:12:39 +0700

    add achievement controller
    # Conflicts:
    #	src/main/java/org/example/sr_group_03_spring_mini_project/controller/AchievementController.java

--- src/main/java/org/example/sr_group_03_spring_mini_project/controller/AchievementController.java
+++ src/main/java/org/example/sr_group_03_spring_mini_project/controller/AchievementController.java
@@ -1,7 +1,17 @@
 package org.example.sr_group_03_spring_mini_project.controller;
 
+import org.example.sr_group_03_spring_mini_project.model.entity.Achievement;
+import org.example.sr_group_03_spring_mini_project.model.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("api/v1/achievements")
 public class AchievementController {
+    public ResponseEntity<List<Achievement>> getAllAchievement(){
+        return null;
+    }
 }
