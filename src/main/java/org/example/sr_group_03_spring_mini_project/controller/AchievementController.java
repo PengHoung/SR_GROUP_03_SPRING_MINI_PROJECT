959fa2db1cc6d829459972f7f445b650899374d9
channsreypich <chhunchansreypich645@gmail.com>
2026-04-05 00:09:35 +0700

    update repository

--- src/main/java/org/example/sr_group_03_spring_mini_project/controller/AchievementController.java
+++ src/main/java/org/example/sr_group_03_spring_mini_project/controller/AchievementController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
@@ -21,8 +22,11 @@ public class AchievementController {
     private final AchievementService achievementService;
 
     @GetMapping
-    public ResponseEntity<ApiResponse<List<Achievement>>> getAllAchievements() {
-        List<Achievement> achievements = achievementService.getAllAchievements();
+    public ResponseEntity<ApiResponse<List<Achievement>>> getAllAchievements(
+            @RequestParam(defaultValue = "1") Integer page,
+            @RequestParam(defaultValue = "10") Integer size
+    ) {
+        List<Achievement> achievements = achievementService.getAllAchievements(page,size);
         return ResponseEntity.ok(
                 ApiResponse.<List<Achievement>>builder()
                         .timestamp(Instant.now())
@@ -35,8 +39,11 @@ public class AchievementController {
     }
 
     @GetMapping("/app-users")
-    public ResponseEntity<ApiResponse<List<Achievement>>> getCurrentUserAchievements() {
-        List<Achievement> achievements = achievementService.getCurrentUserAchievements();
+    public ResponseEntity<ApiResponse<List<Achievement>>> getCurrentUserAchievements(
+            @RequestParam(defaultValue = "1") Integer page,
+            @RequestParam(defaultValue = "10") Integer size
+    ) {
+        List<Achievement> achievements = achievementService.getCurrentUserAchievements(page, size);
         return ResponseEntity.ok(
                 ApiResponse.<List<Achievement>>builder()
                         .timestamp(Instant.now())
